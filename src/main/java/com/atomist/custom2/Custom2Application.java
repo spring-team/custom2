59c18218cce13883156657e8af9abbd448db0e29
Rod Johnson <johnsonroda@gmail.com>
2018-02-17 16:37:25 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/custom2/Custom2Application.java
@@ -0,0 +1,12 @@
+package com.atomist.custom2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Custom2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Custom2Application.class, args);
+	}
+}
